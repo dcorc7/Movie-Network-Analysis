a37c83bca402a183bb9d436056e5df7f5b0db2c6
dcorc7 <dc1524@georgetown.edu>
2025-08-11 22:25:27 -0400

    Website creation, blank pages

--- website/GUdomains.sh
+++ website/GUdomains.sh
@@ -1,7 +1,7 @@
 
 
 echo "Cleaning up existing build..."
-rm -rf FIFA-Position-Classifier
+rm -rf Movie-Network-Analysis
 
 # build website
 echo "Building the website..."
@@ -10,16 +10,16 @@ quarto render
 # Set Correct File Permissions
 echo "Setting correct file permissions..."
 
-find FIFA-Position-Classifier -type f -exec chmod 644 {} \;
+find Movie-Network-Analysis -type f -exec chmod 644 {} \;
 
-find FIFA-Position-Classifier -type d -exec chmod 755 {} \;
+find Movie-Network-Analysis -type d -exec chmod 755 {} \;
 
 # push to the Website
 read -p "Do you want to push the website to your GU domains folder? (y/n): " answer
 
 if [[ "$answer" == "y" || "$answer" == "Y" ]]; then
     echo "Pushing the website to the remote server..."
-    scp -r FIFA-Position-Classifier corcoran@corcoran.georgetown.domains:/home/corcoran/public_html/
+    scp -r Movie-Network-Analysis corcoran@corcoran.georgetown.domains:/home/corcoran/public_html/
     echo "Website successfully pushed!"
 else
     echo "Skipping deployment."
